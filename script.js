dee9ed88d88bf8c474385068a46e795030d21ed5
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-04-09 17:30:51 +0200

    Resolve some problems es 1

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const nameInput = document.getElementById("nameInput");
 const removeButton = document.getElementById("removeButton");
 const form = document.getElementById("registrationForm");
+const savedNameDisplay = document.getElementById("savedNameDisplay");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,6 +11,8 @@ form.addEventListener("submit", (event) => {
     const name = nameInput.value;
     if (name.length > 0) {
       localStorage.setItem("savedName", JSON.stringify(name));
+      savedNameDisplay.textContent = name;
+      nameInput.value = "";
     } else {
       alert("Inserisci un nome valido.");
     }
@@ -20,13 +23,15 @@ form.addEventListener("submit", (event) => {
 const savedName = localStorage.getItem("savedName");
 
 if (savedName) {
-  savedName = JSON.parse(savedName);
-  nameInput.value = savedName;
+  savedNameDisplay.textContent = JSON.parse(savedName);
 }
 
 const removeName = function () {
   localStorage.removeItem("savedName");
+  savedNameDisplay.textContent = "";
   nameInput.value = "";
 };
 
 removeButton.addEventListener("click", removeName);
+
+// Esercizio 2
